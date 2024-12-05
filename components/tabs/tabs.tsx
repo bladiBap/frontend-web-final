f3caa98775dd76374d9886c4c26014a865da9526
bladibap <bladimirbaptistagonzales@gmail.com>
2024-12-05 05:54:08 -0400

    tabs

--- components/tabs/tabs.tsx
+++ components/tabs/tabs.tsx
@@ -1,17 +1,30 @@
 "use client";
-import {Listbox, ListboxItem} from "@nextui-org/react";
+import {Card, ListboxItem} from "@nextui-org/react";
 
 export default function TabsComponent () {
     
     return (
         <div className="flex flex-row">
-            <Listbox
+            <Card
                 aria-label="Listbox Variants"
             >
-                <ListboxItem key="Home1" textValue="Homeasd">Hoasdasme</ListboxItem>
-                <ListboxItem key="Mision1" textValue="Misiones">Misiones</ListboxItem>
-                <ListboxItem key="Logro1" textValue="Logros">Logros</ListboxItem>
-            </Listbox>
+                <Card onPress={
+                    () => {
+                        console.log("Home");
+                    }
+                }>Homae</Card>
+                <Card  onPress={
+                    () => {
+                        console.log("Misiones");
+                    }
+                }>Misiones</Card>
+                <Card onPress={
+                    () => {
+                        console.log("Logros");
+                    }
+                }
+                >Logros</Card>
+            </Card>
         </div>
     );
 }
