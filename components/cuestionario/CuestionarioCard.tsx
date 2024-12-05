206a1d7a35fbe3d6c53774f0292e4b40c9899166
David <davidcitorojas15@gmail.com>
2024-12-05 15:33:55 -0400

    feat: lista de cuestionarios por usuario

---
+++ components/cuestionario/CuestionarioCard.tsx
@@ -0,0 +1,69 @@
+import { Cuestionario } from '@/models/Cuestionario'
+import { CuestionarioService } from '@/services/CuestionarioService';
+import { Button, Card, CardBody, CardFooter, CardHeader } from '@nextui-org/react'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+type Props = {
+    cuestionario: Omit<Cuestionario, "preguntas">;
+    is_editable: boolean;
+}
+
+const CuestionarioCard = (
+    { cuestionario, is_editable = false }: Props
+) => {
+    const queryClient = useQueryClient();
+
+    const deleteCuestionario = useMutation({
+        mutationFn: () => CuestionarioService.deleteCuestionario(cuestionario.id),
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['cuestionarios']
+            });
+            await queryClient.refetchQueries({
+                queryKey: ['cuestionarios']
+            });
+        }
+    })
+
+    return (
+        <Card className='max-w-[300px] w-full'>
+            <CardHeader>
+                <div className='flex justify-between items-center'>
+                    <p className='typography-h2'>
+                        {cuestionario.titulo}
+                    </p>
+                </div>
+            </CardHeader>
+            <CardBody>
+                <p className=''>
+                    {cuestionario.descripcion}
+                </p>
+            </CardBody>
+            {
+                is_editable && (
+                    <CardFooter>
+                        <div className='flex justify-center items-center gap-4 w-full'>
+                            <Link href={`/user/cuestionario/form/${cuestionario.id}`}>
+                                <Button 
+                                    color='primary'
+                                >
+                                    Editar
+                                </Button>
+                            </Link>
+                            <Button 
+                                color='danger'
+                                onPress={() => deleteCuestionario.mutate()}
+                            >
+                                Eliminar
+                            </Button>
+                        </div>
+                    </CardFooter>
+                )
+            }
+        </Card>
+    )
+}
+
+export default CuestionarioCard
